91d2edd167d194a18e1794874bf7c4cd6a4272d5
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-09 20:46:32 +0500

    Refactor todo-list project structure and add InputBox component with styling

---
+++ todo-list/src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+import './InputBox.css';
+
+export function InputBox() {
+  return (
+    <div className='task-box'>
+      <input className="input-box" type="text" placeholder="Add a new task" />
+      <button className='send-button'>Add</button>
+    </div>
+  );
+}
