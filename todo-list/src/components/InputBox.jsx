5028e0566bfb11dc8bc093432a01575af74a1b87
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-10 11:50:20 +0500

    Add InputBox and Messages components; implement styles and initial project setup with Vite

--- todo-list/src/components/InputBox.jsx
+++ todo-list/src/components/InputBox.jsx
@@ -1,11 +1,20 @@
 import './InputBox.css';
+import { useState } from 'react';
 
 export function InputBox() {
+  const [task, setTask] = useState('');
+
+  const handleInputChange = (event) => {
+    setTask(event.target.value);
+  };
+  
+
+
+
   return (
     <>
     <div 
      className="task-box">
-
       <input 
         className="input-box" 
         type="text" 
@@ -13,7 +22,8 @@ export function InputBox() {
       />
 
       <button 
-        className='send-button'>
+        className='send-button'
+        onClick={handleInputChange}>
         Add
       </button>
     </div>
--- todo-list/src/components/Messages.jsx
+++ todo-list/src/components/Messages.jsx
@@ -2,8 +2,8 @@ import './Messages.css';
 
 export function Messages() {
   return (
-    <div className="message-container">
-      <p className="p1">This is the first message</p><br/>
+    <div className="message-container" >
+      <p className="p1">This is the first message </p><br/>
       <p>This is the second message</p>     
     </div>
   )
