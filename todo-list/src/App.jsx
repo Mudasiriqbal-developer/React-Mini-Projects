4918ccd38d11fd9651ea8aae3470e85fd62b0158
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-10 11:05:46 +0500

    Add Messages component and update App structure; modify styles for InputBox and todo-list

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,16 +1,22 @@
 import { TodoList } from "./todo-list";
+import { Messages } from "./components/Messages";
 import { InputBox } from "./components/InputBox";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="app-container">
-        <TodoList />
-        <InputBox />
+    <div className="app-container">
+      <TodoList />
+
+      
+      <div className="todo-area">
+        <Messages />
       </div>
-    </>
+
+      <InputBox />
+    </div>
   );
 }
 
 export default App;
+
---
+++ todo-list/src/components/Messages.jsx
@@ -0,0 +1,10 @@
+import './Messages.css';
+
+export function Messages() {
+  return (
+    <div className="message-container">
+      <p className="p1">This is the first message</p><br/>
+      <p>This is the second message</p>     
+    </div>
+  )
+}
