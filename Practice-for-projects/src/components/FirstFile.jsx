622de2a40f6cd289aa8590ef07942fcacd69b22a
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-11 20:55:55 +0500

    feat: implement FirstFile component and update App structure with new functionality

---
+++ Practice-for-projects/src/components/FirstFile.jsx
@@ -0,0 +1,28 @@
+
+
+export function FirstFile({ count, handleCount, reset, handleAuto }) {
+
+  return (
+    <div>
+      <h1>
+        This is the First File Component
+      </h1>
+
+      <button 
+        onClick={handleCount}>
+        Clicked {count} 
+        times
+      </button>
+
+      <button 
+        onClick={reset}>
+        Clear
+      </button>
+
+      <button onClick={handleAuto}>
+        Auto Click
+      </button>
+
+    </div>
+  )
+}
