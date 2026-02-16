e8140c0d8f418459a722d02d14f10a56dc9f8dca
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-02-16 21:22:51 +0500

    refactor: improve Calculator component structure and fix event handlingr

--- Practice-for-projects/src/components/Calculator.jsx
+++ Practice-for-projects/src/components/Calculator.jsx
@@ -1,23 +1,54 @@
+
+
 export function Calculator() {
 
-  const pressnum = () => {
-    const num1 = document.querySelector(".num1").value;
-    const num2 = document.querySelector(".num2").value;
+
+  const pressNum = () => {
+    const num1 = document.querySelector(".num1");
+    const num2 = document.querySelector(".num2");
     const result = document.querySelector(".result");
-    result.value = parseInt(num1) + parseInt(num2);
-    
+    num1.value;
+    num2.value;
+    result.value = parseInt(num1.value) + parseInt(num2.value);
+  }
+
+  const boxOne = () => {
+    const num1 = document.querySelector(".num1");
+    const btn1 = document.querySelector(".btn1");
+    const btn2 = document.querySelector(".btn2");
+    num1.value = num1.value + btn2.textContent || num1.value + btn1.textContent;
+  }
+
+  const boxTwo = () => {
+    const num2 = document.querySelector(".num2");
+    const btn2 = document.querySelector(".btn2");
+    num2.value = num2.value + btn2.textContent;
   }
 
   return (
-    <div>
+    <>
+      
       <h2>Welcome to my simple but powerful calculator</h2>
 
-      <input type="text" className="num1" onChange={pressnum}/>
-      <h3> + </h3>
-      <input type="text" className="num2" onChange={pressnum}/>
-      <h3> = </h3>
-      <input type="text" className="result" onChange={pressnum}/>
+      <h3> Enter first number </h3>
+      <input type="text" className="num1" />
+      <h3> Enter second number </h3>
+      <input type="text" className="num2" />
+      
+      <h3> Answer </h3>
+      <input type="text" className="result" /><br/>
+      <button onClick={pressNum}>Calculate</button>
 
-    </div>
+      <button className="btn1" onClick={boxOne}>1</button>
+      <button className="btn2" onClick={boxTwo}>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>7</button>
+      <button>7</button>
+      <button>0</button>
+    </>
   );
 }
